e0066a88c5bbf72448b5f05a54e002d904c43e12
Arpit <arpit_jain@live.com>
2022-02-16 16:22:02 +0530

    feat: explain templates
    data binding
    => one way data binding
    => two way data binding using ngModel
    => event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'blogs';
+  title = 'Angular Blogs';
+  subtitle = 'Learning Angular ' + VERSION.major;
+  flag = false;
+  inputField: string = 'test';
+
+  handleSubmit(value: string) {
+    console.log('clicked me.', value)
+    this.flag = !this.flag;
+  }
 }
