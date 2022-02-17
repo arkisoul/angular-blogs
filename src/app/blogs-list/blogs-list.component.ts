0ae831c2c3cfc4e45b34a3caa7b6ab0f3fd8aec4
Arpit <arpit_jain@live.com>
2022-02-17 16:45:02 +0530

    feat: add built-in pipes - date, titlecase

--- src/app/blogs-list/blogs-list.component.ts
+++ src/app/blogs-list/blogs-list.component.ts
@@ -4,6 +4,9 @@ interface Blog {
   title: string;
   description: string;
   isNew: boolean;
+  publishedAt: Date;
+  modifiedAt: Date;
+  author: string;
 }
 
 @Component({
@@ -16,52 +19,82 @@ export class BlogsListComponent implements OnInit {
     {
       title: 'Blog 1',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      isNew: true
+      isNew: true,
+      publishedAt: new Date(),
+      modifiedAt: new Date(),
+      author: 'JOHN DOE'
     },
     {
       title: 'Blog 2',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      isNew: true
+      isNew: true,
+      publishedAt: new Date(),
+      modifiedAt: new Date(),
+      author: 'Jane doe'
     },
     {
       title: 'Blog 3',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      isNew: false
+      isNew: false,
+      publishedAt: new Date(),
+      modifiedAt: new Date(),
+      author: 'John doe'
     },
     {
       title: 'Blog 4',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      isNew: false
+      isNew: false,
+      publishedAt: new Date(),
+      modifiedAt: new Date(),
+      author: 'jonAs Doe'
     },
     {
       title: 'Blog 5',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      isNew: false
+      isNew: false,
+      publishedAt: new Date(),
+      modifiedAt: new Date(),
+      author: 'John Smith'
     },
     {
       title: 'Blog 6',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      isNew: true
+      isNew: true,
+      publishedAt: new Date(),
+      modifiedAt: new Date(),
+      author: 'John Doe'
     },
     {
       title: 'Blog 7',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      isNew: false
+      isNew: false,
+      publishedAt: new Date(),
+      modifiedAt: new Date(),
+      author: 'John Doe'
     },
     {
       title: 'Blog 8',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      isNew: false
+      isNew: false,
+      publishedAt: new Date(),
+      modifiedAt: new Date(),
+      author: 'Sean Pean'
     },
     {
       title: 'Blog 9',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      isNew: false
+      isNew: false,
+      publishedAt: new Date(),
+      modifiedAt: new Date(),
+      author: 'Damian'
     },
     {
       title: 'Blog 10',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      isNew: false
+      isNew: false,
+      publishedAt: new Date(),
+      modifiedAt: new Date(),
+      author: 'Daniel'
     }
   ];
   fruit: string = 'mango';
