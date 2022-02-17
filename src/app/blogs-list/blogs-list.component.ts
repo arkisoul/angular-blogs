3ac3a86e463ab9db1071e2b4d21a7a3d4e16255a
Arpit <arpit_jain@live.com>
2022-02-17 12:50:02 +0530

    feat: attribute directives ngStyle, ngClass, attribute binding

--- src/app/blogs-list/blogs-list.component.ts
+++ src/app/blogs-list/blogs-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 interface Blog {
   title: string;
   description: string;
+  isNew: boolean;
 }
 
 @Component({
@@ -15,46 +16,58 @@ export class BlogsListComponent implements OnInit {
     {
       title: 'Blog 1',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      isNew: true
     },
     {
       title: 'Blog 2',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      isNew: true
     },
     {
       title: 'Blog 3',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      isNew: false
     },
     {
       title: 'Blog 4',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      isNew: false
     },
     {
       title: 'Blog 5',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      isNew: false
     },
     {
       title: 'Blog 6',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      isNew: true
     },
     {
       title: 'Blog 7',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      isNew: false
     },
     {
       title: 'Blog 8',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      isNew: false
     },
     {
       title: 'Blog 9',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      isNew: false
     },
     {
       title: 'Blog 10',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      isNew: false
     }
   ];
   fruit: string = 'mango';
   fruitMango: string = 'mango';
+  titleColor: string = '#ff0000';
+  titleStyle = { 'color': this.titleColor };
   
   constructor() {
     // this.blogs = [];
